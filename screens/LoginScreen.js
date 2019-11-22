be6dc9fe014210b14ae6ce429f0a2b8f689cf69a
Daniel Berekdar <42421210+DanielBerekdar@users.noreply.github.com>
2019-11-22 16:16:58 -0500

    Started login Screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { View, Text, TextInput, TouchableOpacity,StyleSheet} from 'react-native';
+
+
+//LOGIN CONTAINER
+class login extends Component{
+    render() {
+        return (
+            <View style={StyleSheet.container}>
+
+            </View>
+
+        );
+    }
+}
+
+// STYLING 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#99ccff'
+    },
+});
