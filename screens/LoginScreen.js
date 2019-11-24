934e6e88a54f9ec9582b0876f5141e9a7fb8446a
Starlla <claire.cy.t@gmail.com>
2019-11-24 01:58:31 -0500

    Add Login TextInput state and SignUp button

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import { View, Text, TextInput, TouchableOpacity,StyleSheet} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity,StyleSheet, Button} from 'react-native';
+import StaticContainer from "react-native-web/src/vendor/react-native/StaticContainer";
+
 
 
 //LOGIN CONTAINER
@@ -11,18 +13,29 @@ export default class LoginScreen extends React.Component{
     render() {
         return (
             <View style={styles.container}>
-                <Text>Login Screen</Text>
                 <TextInput
                     style={styles.textInput}
                     autoCapitalize="none"
                     placeholder="Email"
+                    onChangeText={email => this.setState({ email })}
+                    value={this.state.email}
                 />
                 <TextInput
                     secureTextEntry
                     style={styles.textInput}
                     autoCapitalize="none"
                     placeholder="Password"
+                    onChangeText={password => this.setState({ password })}
+                    value={this.state.password}
                 />
+                <Button title="Login" onPress={this.handleLogin} />
+                <View style={styles.signUpContainer}>
+                <Text >Don't have an account?</Text>
+                <Button
+                    title="Sign Up"
+                    onPress={() => this.props.navigation.navigate('SignUp')}
+                />
+                </View>
             </View>
 
         );
@@ -43,5 +56,12 @@ const styles = StyleSheet.create({
         borderColor: 'gray',
         borderWidth: 1,
         marginTop: 8
-    }
+    },
+    signUpContainer:{
+        flexDirection: "row",
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 20
+    },
+
 });
