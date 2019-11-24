fbfc44339927d97d491bcd247a148b5ac59d43ec
Claire <claire.cy.t@gmail.com>
2019-11-23 23:18:45 -0500

    Add Loading Screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import LoadingScreen from "../screens/LoadingScreen";
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
-  })
+    Loading: LoadingScreen
+  },
+      //Comment this out or change the initialRoutName to Main when u want to test the main page
+      {
+          initialRouteName: 'Main',
+      }
+
+  )
 );
