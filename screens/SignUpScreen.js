f044f8608be8a5e16d0d6f6cca960ad32418252b
Daniel Berekdar <42421210+DanielBerekdar@users.noreply.github.com>
2019-11-24 21:30:57 -0500

    started signup screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -2,8 +2,29 @@ import React, {Component} from 'react';
 import { View, Text, TextInput, TouchableOpacity,StyleSheet, Button} from 'react-native';
 
 export default class SignUpScreen extends React.Component {
-render(){
-    return <Text>Sign UP</Text>
-}
+    state = { 
+                email: '', 
+                password: '', 
+                first_name: '', 
+                last_name: '', 
+                phone: '', 
+                street1: '', 
+                street2: '', 
+                city: '', 
+                state: '', 
+                zip: '', 
+                errorMessage: null
+            };
 
+    render(){
+        return( 
+        <Text>Sign UP</Text>
+            
+
+
+
+            
+            
+            );
+    }
 }
