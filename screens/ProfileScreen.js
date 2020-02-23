895077a0cde278d6e2926e660c6416935089c324
Claire <claire.cy.t@gmail.com>
2020-02-23 18:17:18 -0500

    Display Name and Email

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import { View, Text, TextInput, TouchableOpacity,StyleSheet, Button} from 'react-native';
 import * as firebase from "firebase";
 
 
 
-export default function ProfileScreen() {
+export default class ProfileScreen extends React.Component {
+    state = {
+        email:"",
+        firstName: ""
+    }
 
-    return (
-        <View>
-          <Button title="LogOut"  onPress={() => firebase.auth().signOut()} />
-        </View>
-    );
+    componentDidMount() {
+        const {email, displayName} = firebase.auth().currentUser;
+        console.log(firebase.auth().currentUser);
+        this.setState({email,displayName});
+    }
+
+    render() {
+        return (
+
+            <View>
+                <Text>Hi {this.state.email}!</Text>
+                <Text>Hi {this.state.displayName}!</Text>
+                <Button title="LogOut" onPress={() => firebase.auth().signOut()}/>
+            </View>
+        );
+    }
 }
+
+
 
 ProfileScreen.navigationOptions = {
   title: 'LogOut',
