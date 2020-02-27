53f2f20f8b81f157dc01306e31d15b2248d304aa
Daniel Berekdar <42421210+DanielBerekdar@users.noreply.github.com>
2020-02-27 18:04:07 -0500

    Added FirebaseConfig
    Fixed issue with initial launch of the application.

---
+++ constants/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCT8JOFI_XwNsAAjBhejHjny5RkHTpUK4s",
+    authDomain: "donationplacesolution.firebaseapp.com",
+    databaseURL: "https://donationplacesolution.firebaseio.com",
+    projectId: "donationplacesolution",
+    storageBucket: "donationplacesolution.appspot.com",
+    messagingSenderId: "1044352397513",
+    appId: "1:1044352397513:web:c53d1211a1037988cbdd18"
+};
+
+export default firebaseConfig;
