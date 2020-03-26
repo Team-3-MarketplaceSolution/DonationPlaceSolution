6a0bdad72b0905437d4a9f36397098b66c3d55af
Claire <claire.cy.t@gmail.com>
2020-03-25 21:37:34 -0400

    Display list number in MyDonation page

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,121 +1,122 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {Platform} from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import EditProfileScreen from "../screens/EditProfileScreen";
-import FormGenerator from  '../screens/AddItem';
+import FormGenerator from '../screens/AddItem';
 import ShippingInfo from '../screens/ShippingInfo';
+import MyDonationScreen from "../screens/MyDonationScreen";
 
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+    web: {headerMode: 'screen'},
+    default: {},
 });
 
 const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  config
+    {
+        Home: HomeScreen,
+    },
+    config
 );
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+    tabBarLabel: 'Home',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-${focused ? '' : '-outline'}`
+                    : 'md-information-circle'
+            }
+        />
+    ),
 };
 
 HomeStack.path = '';
 
 const LinksStack = createStackNavigator(
-  {
-    Links: LinksScreen,
-  },
-  config
+    {
+        Links: LinksScreen,
+    },
+    config
 );
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
+    tabBarLabel: 'Links',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}/>
+    ),
 };
 
 LinksStack.path = '';
 
 const ProfileStack = createStackNavigator(
-  {
-    Profile: ProfileScreen,
-    EditProfile: EditProfileScreen,
+    {
+        Profile: ProfileScreen,
+        EditProfile: EditProfileScreen,
+        MyDonation: MyDonationScreen,
 
-  },
-  config
+    },
+    config
 );
 
 
 ProfileStack.navigationOptions = {
-  tabBarLabel: 'Profile',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'} />
-  ),
+    tabBarLabel: 'Profile',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'}/>
+    ),
 };
 
 ProfileStack.path = '';
 
 // 21341234123
 const ShippingStack = createStackNavigator(
-  {
-    Links: ShippingInfo,
-  },
-  config
+    {
+        Links: ShippingInfo,
+    },
+    config
 );
 ShippingStack.navigationOptions = {
-  tabBarLabel: 'Shipping',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'} />
-  ),
+    tabBarLabel: 'Shipping',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'}/>
+    ),
 };
 
 ShippingStack.path = '';
 
 const ItemStack = createStackNavigator(
-  {
-    Profile: FormGenerator,
-
-  },
-  config
+    {
+        Profile: FormGenerator,
+    },
+    config
 );
 
 
 ItemStack.navigationOptions = {
-  tabBarLabel: 'Items',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'} />
-  ),
+    tabBarLabel: 'Items',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'}/>
+    ),
 };
 
 ItemStack.path = '';
 
 
 const tabNavigator = createBottomTabNavigator({
-  HomeStack,
-  ItemStack,
-  ShippingStack,
-  ProfileStack,
+    HomeStack,
+    ItemStack,
+    ShippingStack,
+    ProfileStack,
 
-  
+
 });
 
 tabNavigator.path = '';
--- screens/MyDonationScreen.js
+++ screens/MyDonationScreen.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, TextInput, TouchableOpacity, View} from 'react-native';
+import * as firebase from "firebase";
+import {
+    Container,
+    List,
+    Content,
+    ListItem,
+    Card,
+    CardItem,
+    Thumbnail,
+    Text,
+    Button,
+    Icon,
+    Left,
+    Body,
+    Right
+} from "native-base";
 
 export default class MyDonationScreen extends React.Component {
+    state = {
+        lists: [],
+    };
+
+    componentDidMount() {
+        let lists=[];
+        const uid = this.props.navigation.getParam('uid');
+        console.log(uid);
+        firebase.database().ref('Lists/'+uid).on('value',(data)=>{
+           data.forEach((childSnapshot) =>{
+               console.log(childSnapshot.key)
+               lists.push(childSnapshot.key);
+            })
+            this.setState({lists:lists});
+        });
+
+    }
 
     render(){
-        return(<View><Text>My Donation</Text></View>);
+        return(
+            <View styel={style.container}>
+                {this.state.lists.map(list => <Card><Text key={list} style={style.list}>List No.{list}</Text></Card>)}
+            </View>
+        );
 }
 
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    list:{
+        textAlign:'center',
+        padding: 15
+    }
+
+})
